3cd943177ef0dd44751902675893df3230b493be
Duong <nguyenduong985911@gmail.com>
2021-10-25 23:48:51 +0700

    main

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var navLogin = document.querySelector('.header__nav-item')
+document.innerHTML = "helo"
+console.log(12);
+// console.log(navLogin);
+// navLogin.onclick = function() {
+//     var modal = document.querySelector('.modal');
+//     console.lo(modal)
+//     modal.style.display = 'flex';
+// }
